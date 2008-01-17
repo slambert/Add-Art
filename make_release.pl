5965e056a1b2118c618e85d13b5a273ccfffccf0
(no author) <(no author)@b7a5d539-5f39-dc11-9877-00e0184e816e>
2008-01-17 01:52:49 +0000

    branch for the last version from dmitri

---
+++ make_release.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use strict;
+
+die "Version number not specified" unless @ARGV;
+
+my $version = $ARGV[0];
+$version =~ s/[^\w\.]//gs;
+
+open(VERSION, ">version");
+print VERSION $ARGV[0];
+close(VERSION);
+
+@ARGV = ("artbanners-$version.xpi");
+do './create_xpi.pl';
