1e0c1f935a15a2f8fe901fe088eed9f6987695da
jamie <jamie@b7a5d539-5f39-dc11-9877-00e0184e816e>
2008-03-13 02:04:09 +0000

    rename all artbanners references to addart (no hyphen; should probably be more consistent)

--- compile.sh
+++ compile.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 # duplicate the working directory
-cp -r artbanners artbanners_temp
-mv artbanners artbanners_renamed && mv artbanners_temp artbanners
-cd artbanners
+cp -r addart addart_working
+mv addart addart_renamed && mv addart_working addart
+cd addart
 
 # strip .svn directories out
 find . -type d -name .svn | xargs rm -rf
 
 # package things up
 cd chrome
-zip -r artbanners.jar content skin
+zip -r addart.jar content skin
 rm -rf content skin
 cd ..
-zip -r ../artbanners-build.xpi .
+zip -r ../addart-build.xpi .
 
 # revert & back out
 cd ..
-rm -rf artbanners
-mv artbanners_renamed artbanners
+rm -rf addart
+mv addart_renamed addart
 exit 0
