499a50939798ccbf689165de9711672e6b7d0f04
jamie <jamie@b7a5d539-5f39-dc11-9877-00e0184e816e>
2008-03-13 02:15:47 +0000

    update compile script to be more descriptive, and also attempt to open the new addart-build.xpi; fix svn:ignore w/ last artbanners reference

--- compile.sh
+++ compile.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 
 # duplicate the working directory
+echo "Duplicating..."
 cp -r addart addart_working
 mv addart addart_renamed && mv addart_working addart
 cd addart
 
 # strip .svn directories out
+echo "Stripping .svn..."
 find . -type d -name .svn | xargs rm -rf
 
 # package things up
+echo "Zippin..."
 cd chrome
-zip -r addart.jar content skin
+zip -rq addart.jar content skin
 rm -rf content skin
 cd ..
-zip -r ../addart-build.xpi .
+zip -rq ../addart-build.xpi .
 
 # revert & back out
+echo "Cleaning up..."
 cd ..
 rm -rf addart
 mv addart_renamed addart
+
+# MacOS can launch it right away for install, too
+echo "Opening w/ Firefox..."
+open -a Firefox addart-build.xpi
+
 exit 0
