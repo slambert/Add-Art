923100ce4bef12e90779b39349c7ecb38fb43bc7
jamie <jamie@b7a5d539-5f39-dc11-9877-00e0184e816e>
2008-03-13 02:40:59 +0000

    revert some console.loggin' that won't work on anybody else's machine

--- addart/components/addart.js
+++ addart/components/addart.js
@@ -207,9 +207,8 @@ const component = {
               frame.setAttribute("class", seed);
               frame.setAttribute("width", width);
               frame.setAttribute("height", height);
-              console.log(context);
-              console.log(context.parentNode);
-              context.parentNode.replaceChild(frame, context);
+              if(context.parentNode)
+                context.parentNode.replaceChild(frame, context);
             }}, 0, timer.TYPE_ONE_SHOT);
           }
         }
