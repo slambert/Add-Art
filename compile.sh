7cb5ab099f419cdabd685a15d4438b303109d142
jamie <jamie@b7a5d539-5f39-dc11-9877-00e0184e816e>
2008-03-13 00:00:14 +0000

    compile script now makes a copy of artbanners dir and strips out .svn directories

--- compile.sh
+++ compile.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-cd artbanners/chrome
+# duplicate the working directory
+cp -r artbanners artbanners_temp
+mv artbanners artbanners_renamed && mv artbanners_temp artbanners
+cd artbanners
+
+# strip .svn directories out
+find . -type d -name .svn | xargs rm -rf
+
+# package things up
+cd chrome
 zip -r artbanners.jar content skin
+rm -rf content skin
 cd ..
 zip -r ../artbanners-build.xpi .
+
+# revert & back out
+cd ..
+rm -rf artbanners
+mv artbanners_renamed artbanners
+exit 0
