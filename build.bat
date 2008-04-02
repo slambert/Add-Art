33d11560ddf7d16b2767f548ad7bf872eca83933
MattK <MattK@b7a5d539-5f39-dc11-9877-00e0184e816e>
2008-04-02 03:52:41 +0000

    revert dejar, but keep the internationalization

--- build.bat
+++ build.bat
@@ -7,9 +7,15 @@ xcopy /s /I addart addart_working
 rem move addart addart_renamed
 rem move addart_working addart
 cd addart_working
-copy https://addons.mozilla.org/de/firefox/downloads/file/19510/adblock_plus-0.7.5.3-fx+tb+sm+fl.xpi
 
 
+echo "zippin"
+cd chrome
+%zf%\7z a -tzip "addart.jar" * -r -mx=0
+rmdir content /S /Q
+rmdir skin /S /Q
+
+cd ..
 %zf%\7z a -tzip "addart-build.xpi" * -r -mx=9
 move addart-build.xpi ../.
 
@@ -17,6 +23,3 @@ cd ..
 rem rmdir addart /S /Q
 rem move addart_renamed addart
 rmdir addart_working /S /Q
-
-
-
