cfea4130b5e340d1747e07ff72ccf4dd05276532
coreytegeler <coreytegeler@gmail.com>
2015-01-10 13:55:09 -0500

    show info in interface

--- data/settings.js
+++ data/settings.js
@@ -1,15 +1,38 @@
 self.port.on("show", function onShow() {
 	
 });
-
-self.port.on("addSubscriptions", function buildInterface(shows) {
-	console.log(shows);
-	// for(var i = 0; i < shows.length; i++) {
-	// 	var show = shows[i];
-	// 	console.log(show[i]);
-	// 	$('#shows').append('<li>'+ show.title +'</li>').addClass(show.title);
-	// 	// var show = shows[i].url;
-	// 	// $('#shows').append('<li></li>');
-	//  //   	$('#shows li').eq(i).append(show);
-	// }
+var subscriptions = [];
+self.port.on("addSubscriptions", function(shows,count) {
+	subscriptions.push(shows);
+	if(subscriptions.length == count) {
+		buildInterface(subscriptions);
+	}
 });
+
+function buildInterface(subs) {
+	var $shows = $('ul#shows');
+	var $showTemplate = $('ul#shows li.show');
+	for(var i = 0; i < subs.length; i++) {
+		addSub(subs[i],i);
+		$shows.append($showTemplate.clone());
+	}
+}
+
+function addSub(show,i) {
+	var $row = $('ul#shows li.show').eq(i);
+	$row.find('h1.title').html(show.title);
+	$row.find('.date').html('Last updated on '+show.date);
+	$row.find('img.thumb').attr('src', show.thumbnail);
+	// var info = {
+	// 	title : show.title,
+	// 	date : show.date,
+	// 	description : show.description,
+	// 	thumbnail : show.thumbnail,
+	// 	link : show.link,
+	// 	url : show.url
+	// };
+
+	// for(var i = 0; i < info.length; i++) {
+	// 	list.append(item);
+	// }
+}
