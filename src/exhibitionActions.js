d1a08df0f65ea64020d4d2a1bcdd42b1e68041b7
Oliver Wise <owise1@gmail.com>
2016-03-25 11:04:14 -0700

    :haircut: v2

---
+++ src/exhibitionActions.js
@@ -0,0 +1,11 @@
+var Reflux = require('reflux')
+
+module.exports = Reflux.createActions([
+    'getExhibitions',
+    'openExhibition',
+    'setExhibition',
+    'addCustomExhibition',
+    'toggleAutoUpdate',
+    'toggleSource',
+    'close'
+])
